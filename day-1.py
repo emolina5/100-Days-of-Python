a6c91cb1e7a76f097fbf805e533917d759c33611
emolina5 <emolinam5@gmail.com>
2022-05-03 17:45:31 +0200

    variable name fix

--- day-1.py
+++ day-1.py
@@ -1,6 +1,6 @@
 # Day 1: Band Name Generator
 print("Welcome to the Band Name Generator.")
-var_input1 = input("What's the name of the city you grew up in? ")
-var_input2 = input("What's your pet's name? ")
+city = input("What's the name of the city you grew up in? ")
+pet = input("What's your pet's name? ")
 
-print("Your band name could be", var_input1, var_input2)
+print("Your band name could be", city, pet)
